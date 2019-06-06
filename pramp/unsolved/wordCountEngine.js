9d8cb0e34a6ac208d566db5db39252cafdef6f2c
Katharine Francis <katie-francis@hotmail.com>
2019-06-06 14:25:08 -0400

    Solved

--- pramp/unsolved/wordCountEngine.js
+++ pramp/unsolved/wordCountEngine.js
@@ -1,14 +1,14 @@
 /*
 Word Count Engine
-Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all 
-unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order. If 
-two or more words have the same count, they should be sorted according to their order in the original sentence. 
-Assume that all letters are in english alphabet. You function should be case-insensitive, so for instance, the 
+Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all
+unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order. If
+two or more words have the same count, they should be sorted according to their order in the original sentence.
+Assume that all letters are in english alphabet. You function should be case-insensitive, so for instance, the
 words “Perfect” and “perfect” should be considered the same word.
 
 The engine should strip out punctuation (even in the middle of a word) and use whitespaces to separate words.
 
-Analyze the time and space complexities of your solution. Try to optimize for time while keeping a polynomial space 
+Analyze the time and space complexities of your solution. Try to optimize for time while keeping a polynomial space
 complexity.
 
 Examples:
@@ -19,9 +19,9 @@ input:  document = "Practice makes perfect. you'll only
 output: [ ["practice", "3"], ["perfect", "2"],
           ["makes", "1"], ["youll", "1"], ["only", "1"],
           ["get", "1"], ["by", "1"], ["just", "1"] ]
-Important: please convert the occurrence integers in the output list to strings (e.g. "3" instead of 3). We ask 
-this because in compiled languages such as C#, Java, C++, C etc., it’s not straightforward to create mixed-type 
-arrays (as it is, for instance, in scripted languages like JavaScript, Python, Ruby etc.). The expected output 
+Important: please convert the occurrence integers in the output list to strings (e.g. "3" instead of 3). We ask
+this because in compiled languages such as C#, Java, C++, C etc., it’s not straightforward to create mixed-type
+arrays (as it is, for instance, in scripted languages like JavaScript, Python, Ruby etc.). The expected output
 will simply be an array of string arrays.
 
 Constraints:
@@ -30,3 +30,22 @@ Constraints:
 [input] string document
 [output] array.array.string
 */
+
+function wordCountEngine(input) {
+  let wordMap = new Map();
+  let neutralized = input
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .split(" ");
+
+  for (let i = 0; i < neutralized.length; i++) {
+    let currentElem = neutralized[i];
+    if (wordMap.has(currentElem)) {
+      let value = Number(wordMap.get(currentElem));
+      wordMap.set(currentElem, (value + 1).toString());
+    } else {
+      wordMap.set(currentElem, "1");
+    }
+  }
+  return Array.from(wordMap).sort((a, b) => b[1] - a[1]);
+}
